695f90b2e6bff979d5db22d1fcdec916c96a568f
Aleksandr Aleksadrov <aav161@yandex.ru>
2020-10-09 14:42:29 +0300

    Finish redux and table logic

--- webpack.config.js
+++ webpack.config.js
@@ -53,7 +53,7 @@ module.exports = {
     }),
     new CopyPlugin({
       patterns: [
-        { 
+        {
           from: path.resolve(__dirname, 'src/favicon.ico'),
           to: path.resolve(__dirname, 'dist')
         }
